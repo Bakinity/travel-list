fd54c110f94b126aec6c2ce2556ec64eff9697d4
Ilqar <bakinitygames.94@gmail.com>
2023-11-01 17:31:03 +0400

    rendering_the_item_list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Charger", quantity: 1, packed: false },
+];
+
 const App = () => {
   return (
-    <div>
-      <h1>Hi</h1>
+    <div className='app'>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
+  );
+};
+
+const Logo = () => {
+  return <h1>🏝️ Far Away 🧳</h1>;
+};
+
+const Form = () => {
+  return (
+    <div className='add-form'>
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+};
+
+const PackingList = () => {
+  return (
+    <div className='list'>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+};
+
+const Stats = () => {
+  return (
+    <footer className='stats'>
+      <em>💼 You have X items on your list,and you already packed X (X%)</em>
+    </footer>
   );
 };
 
